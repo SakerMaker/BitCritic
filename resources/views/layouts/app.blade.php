9fb1b969dc76a68a0f07058579bf66827f05e4f9
Saker <paguyale@gmail.com>
2023-02-23 22:12:14 +0100

    Logo & View

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,21 +7,23 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>{{ config('app.name', 'BitCritic - Video Game Reviewing Community') }}</title>
 
     <!-- Fonts -->
+    <link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
 
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
 </head>
 <body>
-    <div id="app">
+    {{-- <div id="app">
         <nav class="navbar navbar-expand-md navbar-light bg-white shadow-sm">
             <div class="container">
                 <a class="navbar-brand" href="{{ url('/') }}">
-                    {{ config('app.name', 'Laravel') }}
+                    {{ config('app.name', 'BitCritic - Video Game Reviewing Community') }}
                 </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
                     <span class="navbar-toggler-icon"></span>
@@ -75,6 +77,42 @@
         <main class="py-4">
             @yield('content')
         </main>
-    </div>
+    </div> --}}
+
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container">
+          <a class="navbar-brand" href="{{ url('/') }}">
+            <img src="img/Logo Blanco.png" alt="Logo" width="100" height="100"
+              class="d-inline-block align-text-top me-2 img-not-hover">
+            <img src="img/Logo-Blanco2.gif" alt="Logo" width="100" height="100" class="align-text-top me-2 img-hover">
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+            aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav ms-auto">
+              <li class="nav-item">
+                <a class="nav-link nav-link--active" href="{{ url('/') }}">Inicio</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="{{ url('/games') }}">Juegos</a>
+              </li>
+            @guest
+                <li class="nav-item">
+                    <a class="nav-link" href="{{ url('/login') }}">Iniciar Sesión</a>
+                </li>
+            @else
+              <li class="nav-item">
+                <a class="nav-link" href="#">Mi Perfil</a>
+              </li>
+            @endguest
+              <li class="nav-item">
+                <a class="nav-link" href="{{ url('/panel') }}">Panel Administración</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
 </body>
 </html>
