84609f8729e514af4243dc74d409f8cbfc8d8374
Saker <paguyale@gmail.com>
2023-03-06 15:18:43 +0100

    Random Game

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,8 +18,10 @@ class UserSeeder extends Seeder
     public function run()
     {
         //Creacion de 50 cabros(contraseña="jejejeje") y 1 admin(contraseña="adminXDXD")
-        $admin=User::create(['name'=>"admin",'email'=>"admin@gmail.com",'password'=>Hash::make("adminXDXD"),'location'=>"Murcia",'profile_photo_path'=>'img/profileDefault.png','banner_photo_path'=>'img/bannerDefault.png', 'about_you'=>"besto di pesto"]);
-        $admin->assignRole('Admin');
+        $saker=User::create(['name'=>"Saker",'email'=>"sakermaker@gmail.com",'password'=>Hash::make("adminXDXD"),'location'=>"Murcia",'profile_photo_path'=>'img/profileDefault.png','banner_photo_path'=>'img/bannerDefault.png', 'about_you'=>"Illo keloke"]);
+        $victor=User::create(['name'=>"Víctor",'email'=>"victor@gmail.com",'password'=>Hash::make("adminXDXD"),'location'=>"Italia",'profile_photo_path'=>'img/profileDefault.png','banner_photo_path'=>'img/bannerDefault.png', 'about_you'=>"besto di pesto"]);
+        $saker->assignRole('Admin');
+        $victor->assignRole('Admin');
         $usuarios=User::factory(50)->create();
         for($i=0;$i<count($usuarios);$i++){
             $usuarios[$i]->assignRole('User');
