cccfeca883ad0d737b3ffefe861c6b2bac613ed9
Saker <paguyale@gmail.com>
2023-03-06 21:54:37 +0100

    Fix

--- app/Http/Controllers/PerfilController.php
+++ app/Http/Controllers/PerfilController.php
@@ -25,7 +25,7 @@ class PerfilController extends Controller
             $games[]=Game::join("reviews","games.id","=","reviews.id_game")->select("games.id as game_id","games.title as game_title","games.image as game_image","games.genero as game_genero","reviews.id as id_review","reviews.title as review_title","reviews.content as review_content","reviews.created_at as created_at","reviews.updated_at as updated_at")->where("reviews.id","=",$single_review->id)->groupBy("games.id")->get();
         }
 
-        return view('perfil', compact('user'))->with("like",$like)->with("comment",$comment)->with("reviews",$reviews)->with("allreviews",$games);
+        return view('perfil', compact('user'))->with("comment",$comment)->with("reviews",$reviews)->with("allreviews",$games);
     }
 
     public function edit($id) {
