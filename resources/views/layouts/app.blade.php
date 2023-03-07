99ab19bdaf6b8b10a72fe505e3572f3197a3ec6d
Saker <paguyale@gmail.com>
2023-03-07 14:39:04 +0100

    Fix Test

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -17,7 +17,6 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css">
     <!-- Scripts -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-    <link rel="manifest" href="build/manifest.json">
 </head>
 <body>
     {{-- <div id="app">
