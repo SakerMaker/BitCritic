67033fabe9073af3a445fc443cdc82ebacb99c25
Saker <paguyale@gmail.com>
2023-03-07 19:21:27 +0100

    App Fix

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -139,7 +139,7 @@
             <div class="row align-items-center justify-content-between flex-column flex-sm-row">
                 <div class="col-auto"><div class="small m-0 text-white">Copyright &copy; BitCritic 2023</div></div>
                 <div class="col-auto">
-                    <a class="link-light small" href="#!">Política de Privacidad</a>
+                    <a class="link-light small" href="https://forms.gle/nEm4ERmr5WGV5uFG8" target="_blank">Recomendar Juego</a>
                     @guest
                         
                     @else
