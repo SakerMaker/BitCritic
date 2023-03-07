e9770addb0b1e2868c2bc1c8914c0831b0253d2e
Saker <paguyale@gmail.com>
2023-03-07 18:59:15 +0100

    security reasons

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -18,10 +18,6 @@ class UserSeeder extends Seeder
     public function run()
     {
         //Creacion de 50 cabros(contraseña="jejejeje") y 1 admin(contraseña="adminXDXD")
-        $saker=User::create(['name'=>"Saker",'email'=>"saker@gmail.com",'password'=>Hash::make("adminXDXD"),'location'=>"Murcia",'profile_photo_path'=>'img/profileDefault.png','banner_photo_path'=>'img/bannerDefault.png', 'about_you'=>"Illo keloke"]);
-        $victor=User::create(['name'=>"Víctor",'email'=>"victor@gmail.com",'password'=>Hash::make("adminXDXD"),'location'=>"Italia",'profile_photo_path'=>'img/profileDefault.png','banner_photo_path'=>'img/bannerDefault.png', 'about_you'=>"besto di pesto"]);
-        $saker->assignRole('Admin');
-        $victor->assignRole('Admin');
         // $usuarios=User::factory(50)->create();
         // for($i=0;$i<count($usuarios);$i++){
         //     $usuarios[$i]->assignRole('User');
